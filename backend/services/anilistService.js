ed95499b55896abb997f6e651595c386c396adb5
Moisés Fernández <moy-881@hotmail.com>
2024-10-23 18:05:23 -0600

    Method refactor to return a list of anime matching with the title

--- backend/services/anilistService.js
+++ backend/services/anilistService.js
@@ -2,28 +2,34 @@ import anilistClient from "../config/anilistClient.js";
 
 const searchAnimeByTitle = async (title) => {
   const query = `
-    query ($search: String) {
-      Media(search: $search, type: ANIME) {
-        id
-        title {
-          romaji
-          english
-          native
+    query ($search: String, $page: Int, $perPage: Int) {
+      Page(page: $page, perPage: $perPage) {
+        media(search: $search, type: ANIME) {
+          id
+          title {
+            romaji
+            english
+            native
+          }
+          coverImage {
+            large
+          }
+          episodes
+          description
         }
-        coverImage {
-          large
-        }
-        episodes
-        description
       }
     }
   `;
 
-  const variables = { search: title };
+  const variables = {
+    search: title,
+    page: 1,
+    perPage: 10,
+  };
 
   try {
     const data = await anilistClient.request(query, variables);
-    return data.Media;
+    return data.Page.media;
   } catch (error) {
     console.error("Error fetching anime data:", error);
     throw error;
