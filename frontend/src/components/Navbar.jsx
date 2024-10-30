0b687c03886fef763a838cea73e505758f463498
Moisés Fernández <moy-881@hotmail.com>
2024-10-30 17:04:46 -0600

    fix: correct link to for the books section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
           </Link>
 
           <Link
-            to="/"
+            to="/books-section"
             className="text-gray-300 hover:text-blue-400 transition duration-300 ease-in-out"
           >
             Books
