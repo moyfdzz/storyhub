c8d0a6f780cdd57e00463a77136d9d4b50687895
Moisés Fernández <moy-881@hotmail.com>
2024-10-30 14:44:18 -0600

    Added functionality to the sign up page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
 import { motion } from "framer-motion";
+import { useUserStore } from "../stores/useUserStore";
 
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
@@ -11,7 +12,7 @@ const SignUpPage = () => {
     confirmPassword: "",
   });
 
-  const loading = false;
+  const { signup, loading } = useUserStore();
 
   const handleSubmit = (e) => {
     e.preventDefault();
