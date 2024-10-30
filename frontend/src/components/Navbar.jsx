b28ee3be56d6cfaeb34b2f914629015c93fb9c7a
Moisés Fernández <moy-881@hotmail.com>
2024-10-30 15:40:01 -0600

    Added logic to check correctly if the user is admin

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useUserStore } from "../stores/useUserStore";
 
 const Navbar = () => {
   const { user, logout } = useUserStore();
-  const isAdmin = true;
+  const isAdmin = user?.role === "admin";
 
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-blue-800">
