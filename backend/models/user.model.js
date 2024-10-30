1f05fcfe9fc12da35e97c131f4f66c0e34894ca9
Moisés Fernández <moy-881@hotmail.com>
2024-10-30 14:43:51 -0600

    Updated user model to add a role attribute

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -24,6 +24,11 @@ const userSchema = new mongoose.Schema(
       required: true,
       minlength: 6,
     },
+    role: {
+      type: String,
+      enum: ["member", "admin"],
+      default: "member",
+    },
   },
   {
     timestamps: true,
