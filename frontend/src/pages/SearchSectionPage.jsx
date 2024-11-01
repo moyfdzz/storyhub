34010c07a5d9b2aef0047d4317a170168b5dc9f1
Moisés Fernández <moy-881@hotmail.com>
2024-10-31 22:14:04 -0600

    Search section page created with the search bar component

---
+++ frontend/src/pages/SearchSectionPage.jsx
@@ -0,0 +1,11 @@
+import SearchBar from "../components/SearchBar";
+
+const SearchSectionPage = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default SearchSectionPage;
