37890b9c364cac792ab400e674d62532fbececf4
Moisés Fernández <moy-881@hotmail.com>
2024-11-04 15:43:15 -0600

    Added route to get a media

--- backend/routes/media.routes.js
+++ backend/routes/media.routes.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { getMedia } from "../controllers/media.controller.js";
 
 const router = express.Router();
+
+router.post("/getMedia", getMedia);
 
 export default router;
