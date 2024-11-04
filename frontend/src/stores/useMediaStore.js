917474b756f18d355fdb494ede09547c5a838e8a
Moisés Fernández <moy-881@hotmail.com>
2024-11-04 15:41:20 -0600

    Use media store creation for media global state handling

---
+++ frontend/src/stores/useMediaStore.js
@@ -0,0 +1,52 @@
+import { create } from "zustand";
+import axios from "../lib/axios";
+import { toast } from "react-hot-toast";
+
+export const useMediaStore = create((set, get) => ({
+  media: null,
+  loadingState: false,
+
+  getAnime: async (searchTitle) => {
+    try {
+      const res = await axios.post("/media/getMedia", { searchTitle });
+
+      if (res.status === 200) {
+        console.log("Found the media");
+        set({ animeData: res.data, loadingState: false });
+        return res.data;
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        console.log(
+          "User media does not have that anime. Searching externally..."
+        );
+
+        try {
+          const externalRes = await axios.get(`/anime/search/${searchTitle}`);
+
+          set({ animeData: externalRes.data, loadingState: false });
+          return externalRes.data;
+        } catch (externalError) {
+          console.error("Error fetching from external API:", externalError);
+          toast.error(
+            externalError.response?.data?.message ||
+              "An error occurred while fetching from external API"
+          );
+        }
+      } else {
+        set({ loadingState: false });
+        toast.error(error.response?.data?.message || "An error occurred");
+      }
+    }
+  },
+
+  getMedia: async (selectedCategory, searchTitle) => {
+    set({ loadingState: true });
+
+    console.log(selectedCategory);
+
+    if (selectedCategory === "Anime") {
+      await get().getAnime(searchTitle);
+    }
+  },
+}));
