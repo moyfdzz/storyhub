5baac58cd54fa36eb2406d11e6abd3effc7cdbda
Moisés Fernández <moy-881@hotmail.com>
2025-01-10 09:59:08 -0600

    Display media list items

--- frontend/src/pages/SearchSectionPage.jsx
+++ frontend/src/pages/SearchSectionPage.jsx
@@ -1,9 +1,17 @@
 import SearchBar from "../components/SearchBar";
+import { useMediaStore } from "../stores/useMediaStore";
+import MediaListItems from "../components/MediaListItems";
 
 const SearchSectionPage = () => {
+  const { mediaList, loadingState } = useMediaStore();
+
   return (
     <div>
       <SearchBar />
+
+      {!loadingState && mediaList && mediaList.length > 0 && (
+        <MediaListItems mediaListItems={mediaList} />
+      )}
     </div>
   );
 };
